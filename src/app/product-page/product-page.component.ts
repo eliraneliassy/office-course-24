d907cf4003c72e4d046453e4303d60842106b22f
Eliran Eliassy <eliran@e-square.io>
2024-02-22 15:00:56 +0200

    switch map

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -3,12 +3,15 @@ import {ActivatedRoute, Params, Router} from "@angular/router";
 import {FeedService} from "../feed.service";
 import {Book} from "../product.interface";
 import {BookComponent} from "../book/book.component";
+import {Observable, switchMap} from "rxjs";
+import {AsyncPipe} from "@angular/common";
 
 @Component({
   selector: 'app-product-page',
   standalone: true,
   imports: [
-    BookComponent
+    BookComponent,
+    AsyncPipe
   ],
   templateUrl: './product-page.component.html',
   styleUrl: './product-page.component.scss',
@@ -19,14 +22,20 @@ export class ProductPageComponent {
   route = inject(ActivatedRoute);
   feedService = inject(FeedService);
 
-  book?: Book;
+  book$?: Observable<Book | undefined>;
 
   constructor() {
     // console.log(this.route.snapshot.params)
-    this.route.params.subscribe((params: Params) => {
-      this.feedService.getBookById('Harry Potter', params['id'])
-        .subscribe((book: Book | undefined) => this.book = book)
-    });
+    this.book$ = this.route.params
+      .pipe(
+        switchMap((params: Params) =>
+          this.feedService.getBookById('Harry Potter', params['id']))
+
+      )
+    //   .subscribe((params: Params) => {
+    //   this.feedService.getBookById('Harry Potter', params['id'])
+    //     .subscribe((book: Book | undefined) => this.book = book)
+    // });
   }
 
 }
