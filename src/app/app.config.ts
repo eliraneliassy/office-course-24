f493ff60f9ea69883a6276f5c8ced2329f77ae13
Eliran Eliassy <eliran@e-square.io>
2024-02-25 11:10:51 +0200

    material add

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,11 @@ import {PreloadAllModules, provideRouter, withPreloading} from '@angular/router'
 
 import { routes } from './app.routes';
 import {provideHttpClient} from "@angular/common/http";
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes, withPreloading(PreloadAllModules)),
-    provideHttpClient()
+    provideHttpClient(), provideAnimationsAsync()
   ]
 };
